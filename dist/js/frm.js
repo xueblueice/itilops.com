4a4100cd3d6bf2f3e923c76022de6dc9f1ff1272
han.xue <han.xue@atos.net>
2018-06-19 23:24:48 +0800

    initial

---
+++ dist/js/frm.js
@@ -0,0 +1,39 @@
+$(function () {
+
+    $("#defaultForm").submit(function(e){
+        e.preventDefault();
+        var data = $('#defaultForm').serialize();
+
+        var submitData=decodeURIComponent(data,true);
+        var p = '{"id": "id","profile": "intg","from": "itilops@itilops.com","to": "bj_intg@163.com","subject": "Mail from itilops.com","body": "'+submitData+'"}';
+        
+        $.ajax({
+            url:'http://www.itilops.com/api/mail/send',
+            data:p,
+            type: "POST",
+            dataType: "json",
+            contentType: "application/json",
+            cache:false,
+            async:true,
+            beforeSend:function(){
+                
+            },
+            success:function(result){
+                $('#myModal').modal('show');
+            },
+            complete:function(){
+                
+            },
+            error:function(){
+                $('#myModalfailed').modal('show');
+                
+            }
+        })
+       
+
+    });
+
+
+   
+
+});
